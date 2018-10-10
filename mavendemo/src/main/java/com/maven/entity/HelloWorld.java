263c6fbbfc177e9caff248b10c8fb451fe532d80
wzz0827 <297387266@qq.com>
2018-10-10 11:40:50 +0800

    first commit

---
+++ mavendemo/src/main/java/com/maven/entity/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.maven.entity;
+
+public class HelloWorld {
+	public void say() {
+		System.out.println("Holle World");
+	}
+	public String print() {
+		return "Holle World";
+	}
+}
---
+++ mavendemo/src/test/java/com/maven/entity/HolleTest.java
@@ -0,0 +1,11 @@
+package com.maven.entity;
+
+import org.junit.Test;
+
+public class HolleTest {
+	@Test
+	public void test1() {
+		HelloWorld h = new HelloWorld();
+		h.say();
+	}
+}
